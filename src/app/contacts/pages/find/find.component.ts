f865b592f0b0b86c6cc31dbba97e355d77788626
Marcos Albuquerque <mpdsa@pm.me>
2025-03-31 06:18:11 -0300

    Página de localização de contato, estilizado, inserido campo de pesquisa e layout temporário de listagem dos contatos

---
+++ src/app/contacts/pages/find/find.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  templateUrl: "./find.component.html",
+  selector: "findPage"
+})
+export class findComponent {
+  title = "routing-app"
+
+}
