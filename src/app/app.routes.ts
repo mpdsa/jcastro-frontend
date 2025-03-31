9c2c9daa4247d904850e443298ce372fffa88e70
Marcos Albuquerque <mpdsa@pm.me>
2025-03-31 05:36:58 -0300

    Definida rota para página de busca e de cadastro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { findComponent } from './contacts/pages/find.component';
+import { registerComponent } from './contacts/pages/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "buscar",
+    component: findComponent
+  },
+  {
+    path: "cadastrar",
+    component: registerComponent
+  }
+];
